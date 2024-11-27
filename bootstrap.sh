c2aa13ffeba1e2cb115c49f9159cf7d13e9a5a67
Spenser Black <spenserblack01@gmail.com>
2024-11-27 12:25:49 -0500

    Allow forcing linking from `bootstrap.sh`
    For #4

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 SCRIPT_PATH="$(readlink -f $0)"
 DOTFILES="$(dirname "$SCRIPT_PATH")"
+FORCE="false"
+LNARGS="-s"
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-f|--force)
+			FORCE="true"
+			LNARGS="$LNARGS -f"
+			;;
+		*)
+			echo "Unknown option: $1" >&2
+			exit 1
+			;;
+	esac
+	shift
+done
 
 create_symlinks() {
 	DOTFILES_PATH="$1"
@@ -13,11 +29,11 @@ create_symlinks() {
 		DESTDIR="$(dirname "$DEST")"
 		mkdir -p "$DESTDIR"
 		# NOTE: If the file exists, skip creating a conflicting symlink
-		if [ -e "$DEST" ]; then
+		if [ -e "$DEST" ] && [ "$FORCE" != "true" ]; then
 			echo "$DEST exists: skipping $FILENAME" >&2
 			continue
 		fi
-		ln -s "$FILENAME" "$DEST"
+		ln $LNARGS "$FILENAME" "$DEST"
 		echo "Linked: $FILENAME"
 	done
 }
